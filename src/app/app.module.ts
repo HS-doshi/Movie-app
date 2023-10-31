e5b2c0ecfcabc6634e758a29037922073ae9b533
Heet Doshi <doshiheet2872003@gmail.com>
2023-10-31 22:51:32 +0530

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+import { DetailsMoviesComponent } from './details-movies/details-movies.component';
+import { DetailsReviewsComponent } from './details-reviews/details-reviews.component';
+import { NotAuthorizedComponent } from './not-authorized/not-authorized.component';
+import { SearchComponent } from './search/search.component';
+import { LoaderComponent } from './loader/loader.component';
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    DetailsComponent,
+    DetailsMoviesComponent,
+    DetailsReviewsComponent,
+    NotAuthorizedComponent,
+    SearchComponent,
+    LoaderComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
